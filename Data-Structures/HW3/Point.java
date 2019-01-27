7daf56502398ebdb58e79ee7dd99f52b72adb15e
Tyler Nass <tyler.nass30@gmail.com>
2019-01-27 11:46:55 -0600

    Create Point.java

---
+++ Data-Structures/HW3/Point.java
@@ -0,0 +1,61 @@
+package hw3;
+
+/**
+ * A class to that models a 2D point.
+ */
+public class Point {
+	private double x;
+	private double y;
+
+	/**
+	 * Construct the point (<code>x</code>, <code>y</code>).
+	 * @param x the <code>Point</code>'s x coordinate
+	 * @param y the <code>Point</code>'s y coordinate
+	 */
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Move the point to (<code>newX</code>, <code>newY</code>).
+	 * @param newX the new x coordinate for this <code>Point</code>.
+	 * @param newY the new y coordinate for this <code>Point</code>.
+	 */
+	public void moveTo(double newX, double newY) {
+		newX = Math.random()*(400-x);
+		newY = Math.random()*(400-y);
+	}
+
+	/**
+	 * Returns the point's x coordinate
+	 * @return the point's x coordinate
+	 */
+	public double getX() {
+		return x;
+	}
+
+	/**
+	 * Returns the point's y coordinate
+	 * @return the point's y coordinate
+	 */
+	public double getY() {
+		return y;
+	}
+	
+	/**
+	 * Returns the distance between <code>this Point</code> and the <code>Point p2</code>
+	 * @param p2 the other <code>Point</code>
+	 * @return the distance between <code>this Point</code> and the <code>Point p2</code>
+	 */
+	public double distance(Point p2) {
+		//TODO - This code is wrong and needs to be changed
+
+		double x1 = p2.getX();
+		double y1 = p2.getY();
+		double x2 = this.x;
+		double y2 = this.y;
+		double dist = Math.sqrt(Math.pow((x1-x2), x) + Math.pow((y1-y2), y));
+		return dist;
+	}
+}
